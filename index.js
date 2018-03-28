4d7cf3bc46d44eab3d24835fbe77a9c5458f911a
Ilho Kim <ilhokim@Ilhoui-MacBook-Pro.local>
2018-03-27 22:44:14 -0700

    Comment out rocket for travelers

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-const redBlock = document.querySelector('.block--red');
-const blueBlock = document.querySelector('.block--blue');
-const greenBlock = document.querySelector('.block--green');
-const pinkBlock = document.querySelector('.block--pink');
-const grayBlock = document.querySelector('.block--gray');
+// const redBlock = document.querySelector('.block--red');
+// const blueBlock = document.querySelector('.block--blue');
+// const greenBlock = document.querySelector('.block--green');
+// const pinkBlock = document.querySelector('.block--pink');
+// const grayBlock = document.querySelector('.block--gray');
 
-const rocket = (event) => {
-  event.stopPropagation();
-  event.target.style.order = "-5";
-}
+// const rocket = (event) => {
+//   event.stopPropagation();
+//   event.target.style.order = "-5";
+// }
 
-redBlock.addEventListener('click', rocket);
-blueBlock.addEventListener('click', rocket);
-greenBlock.addEventListener('click', rocket);
-pinkBlock.addEventListener('click', rocket);
-grayBlock.addEventListener('click', rocket);
+// redBlock.addEventListener('click', rocket);
+// blueBlock.addEventListener('click', rocket);
+// greenBlock.addEventListener('click', rocket);
+// pinkBlock.addEventListener('click', rocket);
+// grayBlock.addEventListener('click', rocket);
+
